a10a6bd3826d75af50374329943877c3a7c9f1d7
Goose <canonicduck@tuta.io>
2020-06-25 16:34:32 +0300

    commit 2

--- src/task2/ImageToPGM.java
+++ src/task2/ImageToPGM.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -14,42 +16,52 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class ImageToPGM extends Application {
-
-
     @Override
-    public void start(Stage primaryStage) {
-        VBox vb = initInterface();
-        primaryStage.setScene(new Scene(vb, 640, 800));
+    public void start(Stage primaryStage) throws FileNotFoundException {
+        primaryStage.setScene(new Scene(initInterface(primaryStage), 640, 800));
         primaryStage.show();
+    }
 
+    private VBox initInterface(Stage primaryStage) {
+        VBox window = new VBox();
+
+        ImageView iv = new ImageView();
+        Label l = new Label();
         Button b = new Button("Choose File");
-        vb.getChildren().addAll(b);
         FileChooser fc = new FileChooser();
         b.setOnAction(e -> {
             File chImg = fc.showOpenDialog(primaryStage);
-            Label l = new Label(chImg.getName());
-            vb.getChildren().add(l);
+            l.setText(chImg.getName());
+
+            Image im;
+            PixelReader pr;
+
+            try {
+                FileInputStream fis = new FileInputStream(chImg);
+                im = new Image(fis);
+                pr = im.getPixelReader();
+
+                int w = (int) im.getWidth();
+                int h = (int) im.getHeight();
+                WritableImage im7 = new WritableImage(w, h);
+                for (int i = 0; i < w; i++) {
+                    for (int j = 0; j < h; j++) {
+                        im7.getPixelWriter().setColor(i, j, pr.getColor(i, j));
+                    }
+                }
+                iv.setImage(im7);
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+
+
         });
 
-    }
-
-    private VBox initInterface() {
-        VBox window = new VBox();
-        HBox h1 = new HBox();
-
-        ImageView iv1 = new ImageView();
-        WritableImage im1 = new WritableImage(120, 100);
-        for (int i = 0; i < 120; i++) {
-            for (int j = 0; j < 100; j++) {
-                im1.getPixelWriter().setColor(i,j, Color.GREEN);
-            }
-        }
-        iv1.setImage(im1);
-
-        h1.getChildren().addAll(iv1);
-        window.getChildren().addAll(h1);
+        window.getChildren().addAll(b, l, iv);
         return window;
     }
 
