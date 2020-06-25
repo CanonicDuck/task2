d77bb045bb83ce3ce96c6cbd0cb1ca121241795d
Goose <canonicduck@tuta.io>
2020-06-25 18:01:44 +0300

    final commit v1

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,8 @@
 module task2 {
     requires javafx.controls;
     requires javafx.graphics;
+    requires java.desktop;
+    requires javafx.swing;
 
     opens task2;
 }
--- src/task2/ThirdTask.java
+++ src/task2/ThirdTask.java
@@ -6,14 +6,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.*;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
 import java.io.File;
-import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import javafx.embed.swing.SwingFXUtils;
 
 public class ThirdTask extends Application {
     @Override
@@ -43,22 +46,29 @@ public class ThirdTask extends Application {
                 int h = Integer.parseInt(sc.next());
                 sc.next();
 
+                WritableImage wi = new WritableImage(w, h);
+
                 pic = new int[w][h];
-                for (int i = 0; i < w; i++) {
-                    for (int j = 0; j < h; j++) {
+                for (int i = 0; i < h; i++) {
+                    for (int j = 0; j < w; j++) {
                         pic[i][j] = Integer.parseInt(sc.next());
+                        wi.getPixelWriter().setColor(j, i, Color.gray((double) pic[i][j]/255));
                         System.out.print(pic[i][j] + " ");
                     }
                     System.out.println();
                 }
+                iv.setImage(wi);
 
-
-
+                File file = new File("test.png");
+                RenderedImage renderedImage = SwingFXUtils.fromFXImage(wi, null);
+                ImageIO.write(renderedImage, "png", file);
 
             } catch (Exception fileNotFoundException) {
                 fileNotFoundException.printStackTrace();
             }
         });
+
+
 
         window.getChildren().addAll(b, l, iv);
         return window;
